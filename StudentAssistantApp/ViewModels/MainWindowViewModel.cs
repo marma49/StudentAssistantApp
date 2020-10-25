0a49b6b6236eea72534900fe11d967166dd11ff8
Serhii Buriak <sergeyburiak55@gmail.com>
2020-10-25 18:37:24 +0100

    Created Views and ViewModels for every menu item

--- StudentAssistantApp/ViewModels/MainWindowViewModel.cs
+++ StudentAssistantApp/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Windows;
 using Caliburn.Micro;
 
 namespace StudentAssistantApp.ViewModels
 {
-    public class MainWindowViewModel : Screen 
+    public class MainWindowViewModel : Conductor<object>
     {
+
+        public void LoadNotes()
+        {
+            ActivateItem(new NotesWindowViewModel());
+        }
+
+        public void LoadGrades()
+        {
+            ActivateItem(new GradesWindowViewModel());
+        }
+
+        public void LoadTasks()
+        {
+            ActivateItem(new TasksWindowViewModel());
+        }
+
+        public void LoadCalendar()
+        {
+            ActivateItem(new CalendarWindowViewModel());
+        }
+
+        public void LoadOptions()
+        {
+            ActivateItem(new OptionsWindowViewModel());
+        }
+
     }
 }
---
+++ StudentAssistantApp/ViewModels/NotesWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentAssistantApp.ViewModels
+{
+    public class NotesWindowViewModel : Screen
+    {
+    }
+}
