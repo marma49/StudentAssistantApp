c141a451e5e503b81bc7474f1d856453cefd46e5
Serhii Buriak <sergeyburiak55@gmail.com>
2020-11-08 23:20:47 +0100

    Modified task window, added pop-up to add new task.

--- StudentAssistantApp/ViewModels/TasksWindowViewModel.cs
+++ StudentAssistantApp/ViewModels/TasksWindowViewModel.cs
@@ -9,23 +9,42 @@ namespace StudentAssistantApp.ViewModels
     class TasksWindowViewModel : Screen
     {
         private BindableCollection<TaskModel> tasks = new BindableCollection<TaskModel>();
+        private string taskName;
+        private string taskExplanation;
 
         public BindableCollection<TaskModel> Tasks
         {
             get { return tasks; }
             set { tasks = value; }
         }
+        public string TaskName
+        {
+            get { return taskName; }
+            set { taskName = value;
+                NotifyOfPropertyChange("TaskName");
+            }
+        }
+
+        public string TaskExplanation
+        {
+            get { return taskExplanation; }
+            set { taskExplanation = value;
+                NotifyOfPropertyChange("TaskExplanation");
+            }
+        }
 
         public TasksWindowViewModel()
         {
-            Tasks.Add(new TaskModel { TaskName = "Skończyć projekt z programowania", TaskExplanation="Skończyć zadanie domowe z programowania"}); 
+            Tasks.Add(new TaskModel { TaskName = "Skończyć projekt", TaskExplanation="Skończyć zadanie domowe z programowania"}); //TODO Сделать ограничение на длинну названия задания
             Tasks.Add(new TaskModel { TaskName = "Impreza", TaskExplanation="Napić się piwka!"}); 
             Tasks.Add(new TaskModel { TaskName = "Impreza", TaskExplanation="Napić się piwka!"}); 
         }
 
         public void AddNewTask()
         {
-            tasks.Add(new TaskModel { TaskName = "Nowe dziwne zadanie", TaskExplanation="Jakaś dziwna definicja, dziwnego zadania! xD"});
+            tasks.Add(new TaskModel { TaskName = taskName, TaskExplanation = taskExplanation });
+            TaskExplanation = "";
+            TaskName = "";
         }
     }
 }
