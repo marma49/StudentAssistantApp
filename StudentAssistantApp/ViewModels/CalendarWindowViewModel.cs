21f7fd10441632c94097a8fcd31dd57750c653fc
Serhii Buriak <sergeyburiak55@gmail.com>
2020-11-29 20:09:35 +0100

    Changed CalendarWindow added new calendar and Events

--- StudentAssistantApp/ViewModels/CalendarWindowViewModel.cs
+++ StudentAssistantApp/ViewModels/CalendarWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using StudentAssistantApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,30 @@ namespace StudentAssistantApp.ViewModels
 {
     class CalendarWindowViewModel : Screen
     {
+        BindableCollection<EventModel> events = new BindableCollection<EventModel>();
+        public BindableCollection<EventModel> Events { get => events; private set => events = value; }
+
+        private DateTime selectedDate;
+
+        public DateTime SelectedDate
+        {
+            get { return selectedDate; }
+            set { selectedDate = value; NotifyOfPropertyChange("SelectedDate"); }
+        }
+
+        public CalendarWindowViewModel()
+        {
+            SelectedDate = DateTime.Now;
+            Events.Add(new EventModel { EventName = "The First Event Name!", EventDate = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")});
+            Events.Add(new EventModel { EventName = "The First Event Name!", EventDate = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")});
+            Events.Add(new EventModel { EventName = "The First Event Name!", EventDate = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")});
+            Events.Add(new EventModel { EventName = "The First Event Name!", EventDate = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt")});
+        }
+
+        public void AddEvent()
+        {
+            Events.Add(new EventModel { EventName = "The First Event Name!", EventDate = SelectedDate.ToString("MM/dd/yyyy hh:mm tt")});
+        }
+
     }
 }
