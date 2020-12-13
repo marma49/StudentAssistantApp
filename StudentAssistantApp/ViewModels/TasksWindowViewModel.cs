641961fe18e34a6a413ba866e1ffc4f714a3fde6
michalkopera <michalikpc@outlook.com>
2020-12-13 23:37:01 +0100

    CRUD dla Tasks

--- StudentAssistantApp/ViewModels/TasksWindowViewModel.cs
+++ StudentAssistantApp/ViewModels/TasksWindowViewModel.cs
@@ -18,7 +18,7 @@ namespace StudentAssistantApp.ViewModels
         private string taskName;
         private string taskExplanation;
         private bool isDialogOpen, isEditing;
-        private int itemIndex, itemCount = 0;
+        private int itemIndex, itemCount = 1;
 
         public BindableCollection<TaskModel> Tasks
         {
@@ -52,9 +52,21 @@ namespace StudentAssistantApp.ViewModels
 
         public TasksWindowViewModel()
         {
-            Tasks.Add(new TaskModel { TaskName = "Skończyć projekt", TaskExplanation="Skończyć zadanie domowe z programowania", TaskID = itemCount++}); //TODO Сделать ограничение на длинну названия задания
-            Tasks.Add(new TaskModel { TaskName = "Styudia", TaskExplanation="Zrobić zadanie z programowania!", TaskID = itemCount++ }); 
-            Tasks.Add(new TaskModel { TaskName = "Impreza", TaskExplanation="Napić się piwka!", TaskID = itemCount++});
+            //Tasks.Add(new TaskModel { TaskName = "Skończyć projekt", TaskExplanation="Skończyć zadanie domowe z programowania", TaskID = itemCount++}); //TODO Сделать ограничение на длинну названия задания
+            //Tasks.Add(new TaskModel { TaskName = "Styudia", TaskExplanation="Zrobić zadanie z programowania!", TaskID = itemCount++ }); 
+            //Tasks.Add(new TaskModel { TaskName = "Impreza", TaskExplanation="Napić się piwka!", TaskID = itemCount++});
+            
+            //Czytanie z bazy i wyświetlenie
+           using(var context = new StudentAppContext())
+            {
+                var tasks = context.DBTasks.ToList();
+                foreach(DBTask t1 in tasks)
+                {
+                    Tasks.Add(new TaskModel { TaskName = t1.TaskHeadline, TaskExplanation = t1.TaskText, TaskID = t1.DBTaskId });
+                }
+
+                //return context.DBTasks.ToList();
+            }
         }
 
         public void CloseDialog()
@@ -71,6 +83,16 @@ namespace StudentAssistantApp.ViewModels
                 Tasks[itemIndex].TaskName = TaskName;
                 Tasks[itemIndex].TaskExplanation = TaskExplanation;
                 Tasks.Refresh(); //Raises a change notification indicating that all bindings should be refreshed.
+
+                //Modyfikacja w bazie
+                using (var context = new StudentAppContext())
+                {
+                    DBTask DBTask = context.DBTasks.Where(x => x.DBTaskId == 4).FirstOrDefault(); //Do poprawy idIndex
+                    DBTask.TaskHeadline = TaskName;
+                    DBTask.TaskText = TaskExplanation;
+
+                    context.SaveChanges();
+                }
             }
             else
             {
@@ -100,24 +122,35 @@ namespace StudentAssistantApp.ViewModels
 
             await Task.Delay(300);//A short delay to make more smooth animation of removing
             Tasks.Remove(task); //Get and remove selected task
-            if (Tasks.Count == 0)
+            /*if (Tasks.Count == 0)
             {
                 itemCount = 0;
+            }*/
+
+            //Usuniecie z bazy
+            using (var context = new StudentAppContext())
+            {
+                DBTask DBTask = context.DBTasks.Where(x => x.DBTaskId == itemIndex).FirstOrDefault();
+                context.DBTasks.Remove(DBTask);
+
+                context.SaveChanges();
             }
         }
         public void EditTask(object sender)
         {
             var listBoxItem = sender as System.Windows.Controls.ListBoxItem; //Get sender
             itemIndex = int.Parse(listBoxItem.Tag.ToString()); //Convert  listBoxItem.Tag to int (TaskID)
+            int itemindex = itemIndex;
             TaskModel task = Tasks.Where(n => n.TaskID == itemIndex).First();
             itemIndex = Tasks.IndexOf(task);
-            
+
             IsDialogOpen = true;
             isEditing = true;
 
             TaskName = Tasks[itemIndex].TaskName;
             TaskExplanation = Tasks[itemIndex].TaskExplanation;
+
+            
         }
-     
     }
 }
