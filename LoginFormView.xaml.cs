1c61f8f3e53cdef70d89043deab6a0f8fea467e8
kmfran <73110907+kmfran@users.noreply.github.com>
2021-01-19 15:54:28 +0100

    Add files via upload

---
+++ LoginFormView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentAssistantApp.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginFormView.xaml
+    /// </summary>
+    public partial class LoginFormView : Window
+    {
+        public LoginFormView()
+        {
+            InitializeComponent();
+        }
+        //teoretycznie złamanie zasad MVVM, ale jest konieczne by działało logowanie
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((dynamic)this.DataContext).SecurePassword = ((PasswordBox)sender).SecurePassword;
+            }
+        }
+    }
+}
