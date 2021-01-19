da7854749101a03934ebca099f897cb04d616228
kmfran <73110907+kmfran@users.noreply.github.com>
2021-01-19 17:18:13 +0100

    Update LoginFormView.xaml.cs

--- StudentAssistantApp/Views/LoginFormView.xaml.cs
+++ StudentAssistantApp/Views/LoginFormView.xaml.cs
@@ -21,12 +21,26 @@ namespace StudentAssistantApp.Views
         {
             InitializeComponent();
         }
-        //teoretycznie złamanie zasad MVVM, ale jest konieczne by działało logowanie
+        //teoretycznie złamanie zasad MVVM, ale jest konieczne by działało logowanie i tworzenie użytkownika
         private void Password_PasswordChanged(object sender, RoutedEventArgs e)
         {
             if (this.DataContext != null)
             {
                 ((dynamic)this.DataContext).SecurePassword = ((PasswordBox)sender).SecurePassword;
+            }
+        }
+        private void NewPassword1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((dynamic)this.DataContext).NewSecurePassword1 = ((PasswordBox)sender).SecurePassword;
+            }
+        }
+        private void NewPassword2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((dynamic)this.DataContext).NewSecurePassword2 = ((PasswordBox)sender).SecurePassword;
             }
         }
     }
